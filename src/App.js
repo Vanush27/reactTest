5b8d0a6c17cef611a6aa5497fcdd752e13a995e2
Vanush27 <vanushgrigoryan90gmail.com>
2021-12-15 19:16:21 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div>
+                <h1 className="title">Real time</h1>
+                <h2 className="date">It is {new Date().toLocaleTimeString()}.</h2>
+
+        </div>
+    );
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+function tick() {
+    ReactDOM.render(
+        <App />,
+        document.getElementById('root')
+    );
+}
+setInterval(tick, 1000);
